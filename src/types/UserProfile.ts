26726f38bcfca5c85309f240ce31cb43d6f67831
Shane O Sullivan <shaneosullivan@Shanes-MacBook-Air.local>
2025-05-27 21:08:26 +0800

    chore: trigger clean build after verifying UserProfile type

---
+++ src/types/UserProfile.ts
@@ -0,0 +1,18 @@
+export interface UserProfile {
+  activePlanId?: string;
+  id?: string;
+  uid: string;
+  displayName?: string;
+  email?: string;
+  bio?: string;
+  fitnessLevel?: "beginner" | "intermediate" | "advanced" | "elite";
+  goals?: string[];
+  height?: number;
+  weight?: number;
+  birthdate?: Date | { seconds: number; nanoseconds: number };
+  sex?: "male" | "female" | "other" | "prefer-not-to-say";
+  interests?: string[];
+  completedSetup?: boolean;
+  streak?: number;
+  weeklyGoal?: number;
+}
